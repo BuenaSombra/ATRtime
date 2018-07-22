18fc4dfa480d2b34fffe9c15535cd846012cf7a4
Yo <buenasombra@outlook.es>
2018-07-22 02:54:40 +0200

    preparacion crear pagina nueva actividad

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl:   NavController,
+              public modalCtrl: ModalController) {
 
+  }
+
+  nuevaActividad() {
+    console.log("en nuevaActividad()");
+    let modalNuevaActividad = this.modalCtrl.create(NuevaActividadPage);
+    /*
+    modalNuevaActividad.present();
+    // código para cuando se cierre la pantalla modal de nueva actividad
+    modalNuevaActividad.onDidDismiss(actividad => {
+      if (actividad) {
+        console.log("me devuelve la actividad " + actividad);
+        this.listaActividades.push(actividad);
+      }
+    });
+    */
   }
 
 }
